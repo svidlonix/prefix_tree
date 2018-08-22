93c43b50c3a109161aeb29fe138765c8a2ce6aa5
svidlonix <sergey.svidlo@nixsolutions.com>
2018-08-22 18:26:38 +0300

    feature add functionality save load file

--- spec/load_save_file.spec.rb
+++ spec/load_save_file.spec.rb
@@ -1,15 +1,34 @@
 require 'spec_helper'
 
 describe LoadSaveFile do
-  let(:file) { LoadSaveFile.new('file_name') }
-  subject { file }
+  let(:file_name) { 'spec/test_data_file.txt' }
 
-  describe '#save_to_file' do
-    it { expect(file.save_to_file).to eq('file_name') }
+  describe '.save' do
+    before do
+      Word.new('catty').add
+      Word.new('cat').add
+      LoadSaveFile.save(file_name)
+    end
+
+    it { expect(File.open(file_name, 'r').read.split("\n")).to eq(%w[catty cat]) }
   end
 
-  describe '#load_from_file' do
-    it { expect(file.load_from_file).to eq('file_name') }
+  describe '.load' do
+    let(:expected_result) do
+      {
+        'c' => { 'a' => { 't' => { last_char: true } } },
+        'd' => { 'o' => { 'g' => { last_char: true } } }
+      }
+    end
+
+    before do
+      Word.new('cat').add
+      Word.new('dog').add
+      LoadSaveFile.save(file_name)
+      LoadSaveFile.load(file_name)
+    end
+
+    it { expect($prefix_tree).to eq(expected_result) }
   end
 
   describe '#save_to_zip_file' do
